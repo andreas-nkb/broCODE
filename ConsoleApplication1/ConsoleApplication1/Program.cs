bc06e4117c67095018c2eecc16896820ddecd037
andreas-nkb <andreas.nkb@gmail.com>
2016-08-28 21:40:44 +0200

    v 0.2.1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ConsoleApplication1
 {
-    class Program
-    {
-        static void Main(string[] args)
+
+        public partial class Form1 : Form
         {
-            Console.WriteLine("Hej Crippa!");
-            Console.Beep();
-            Console.WriteLine("Hej Joxx!");
-            Console.ReadLine();
-            
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.ShowDialog();
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName;
+                    fileName = dlg.FileName;
+                    MessageBox.Show(fileName);
+                }
+            }
         }
-    }
 }
